d1c45c0e0654c7d7522c4a8550acfca81567f9db
Lindsay M <lindsaymecher@gmail.com>
2019-12-18 23:28:36 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,23 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  return array.index(value_to_find)
+  
 end
+
+
+
+
 
 def find_max_value(array)
-  # Add your solution here
+  array.sort 
+  return array.max 
 end
 
+
+
 def find_min_value(array)
-  # Add your solution here
+  array.sort.reverse
+  return array.min
 end
+
+
+
