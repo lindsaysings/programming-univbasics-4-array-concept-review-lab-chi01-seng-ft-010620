a9c663cf9cff899328d885c4dee27c3d5577a717
Lindsay M <lindsaymecher@gmail.com>
2019-12-18 23:02:44 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,15 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.each_with_index do |number, index|
+    if value_to_find == number
+      return index 
+    end
+  end
+nil
 end
+
+
+
+
 
 def find_max_value(array)
   # Add your solution here
@@ -9,3 +18,17 @@ end
 def find_min_value(array)
   # Add your solution here
 end
+
+
+a = 1 
+
+while a <= 1000 do
+  puts a 
+  a += 1 
+end 
+
+
+
+
+
+array = [1,2,3,4]
