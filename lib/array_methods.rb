b40e6136eb0f57be027d409ebc0f5c539e0097e6
Lindsay M <lindsaymecher@gmail.com>
2019-12-18 23:27:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,14 +9,14 @@ end
 
 def find_max_value(array)
   array.sort 
-  return array[-1] 
+  return array.max 
 end
 
 
 
 def find_min_value(array)
   array.sort.reverse
-  return array[-1]
+  return array.min
 end
 
 
